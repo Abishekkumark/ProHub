f838c9c6b0586e5a1797db35e67946599331451b
Abishek Kumar <111441641+Abishekkumark@users.noreply.github.com>
2023-10-21 10:18:27 +0530

    Login.js

---
+++ Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div className="login-container">
+      <h1>Login</h1>
+      <form>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" name="username" required />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" required />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
